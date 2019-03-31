681d2def11a9155dba6710991ba30e7763438119
Ivan <cellarrainbow@gmail.com>
2019-03-31 14:57:07 +0300

    Updates

---
+++ precache-manifest.fdbac2866e11650c8c9ada787cd094d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d5ab06af72150585467",
+    "url": "/StarWarsdb/static/js/runtime~main.9d5ab06a.js"
+  },
+  {
+    "revision": "63acc3b1459e0dd38d46",
+    "url": "/StarWarsdb/static/js/main.63acc3b1.chunk.js"
+  },
+  {
+    "revision": "6a60467a33094c13909c",
+    "url": "/StarWarsdb/static/js/2.6a60467a.chunk.js"
+  },
+  {
+    "revision": "63acc3b1459e0dd38d46",
+    "url": "/StarWarsdb/static/css/main.83d37a87.chunk.css"
+  },
+  {
+    "revision": "b0c27c01b28a93de337d8687baf9564e",
+    "url": "/StarWarsdb/index.html"
+  }
+];
