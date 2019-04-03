4f1c5097285e9a6874e7af45633a4fcf61052043
Ivan <cellarrainbow@gmail.com>
2019-04-03 14:38:13 +0300

    Updates

---
+++ precache-manifest.3e524e031ea73be2e8504d36b54ef475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c58310ef443c370025b5",
+    "url": "/swdatabase/static/js/runtime~main.c58310ef.js"
+  },
+  {
+    "revision": "a00a5e52c2cbab76f831",
+    "url": "/swdatabase/static/js/main.a00a5e52.chunk.js"
+  },
+  {
+    "revision": "4705cf6ee6edc80ab50f",
+    "url": "/swdatabase/static/js/2.4705cf6e.chunk.js"
+  },
+  {
+    "revision": "a00a5e52c2cbab76f831",
+    "url": "/swdatabase/static/css/main.85d7304d.chunk.css"
+  },
+  {
+    "revision": "5771ad497509a9193ecfa89ad14dd3f2",
+    "url": "/swdatabase/index.html"
+  }
+];
